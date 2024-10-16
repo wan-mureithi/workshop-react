0c303260703a6073041ee26ba50ad9d403209162
wan.mureithi <estherdee27@gmail.com>
2024-10-16 22:41:14 +0300

    install storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
