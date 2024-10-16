fad7933f8fd64cce7528ca7b07d4964ddece0808
wan.mureithi <estherdee27@gmail.com>
2024-10-16 18:04:15 +0300

    remove stuff

--- src/model/utils.ts
+++ src/model/utils.ts
@@ -4,7 +4,3 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
-
-function strip(string: string) {
-  return string.replace(/^\s+|\s+$/g, "")
-}
